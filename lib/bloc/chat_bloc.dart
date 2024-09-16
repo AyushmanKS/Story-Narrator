b8f9371a15bd96163e93c4f9857cfd3c9ebcb15e
AyushmanKS <ayushmanks1845@gmail.com>
2024-09-16 21:23:56 +0530

    update: bloc chat/event/state

--- lib/bloc/chat_bloc.dart
+++ lib/bloc/chat_bloc.dart
@@ -1,16 +1,28 @@
+import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:story_narrator/models/message_model.dart';
+import 'package:story_narrator/repos/chat_repo.dart';
 
 part 'chat_event.dart';
 part 'chat_state.dart';
 
 class ChatBloc extends Bloc<ChatEvent, ChatState> {
   ChatBloc() : super(ChatInitial()) {
-    on<ChatEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<ChatGenerateNewTextMessageEvent>(chatGenerateNewTextMessageEvent);
+  }
 
-    List<MessageModel> messages = [];
+  List<MessageModel> messages = [];
+
+  FutureOr<void> chatGenerateNewTextMessageEvent(
+      ChatGenerateNewTextMessageEvent event, Emitter<ChatState> emit) async {
+    messages.add(
+      (MessageModel(
+        role: "user",
+        parts: [ChatPartModel(text: event.inputMessage)],
+      )),
+    );
+    emit(ChatSuccessState(messages: messages));
+    await ChatRepo.chatTextGenerationRepo(messages);
   }
 }
--- lib/bloc/chat_state.dart
+++ lib/bloc/chat_state.dart
@@ -4,3 +4,8 @@ part of 'chat_bloc.dart';
 sealed class ChatState {}
 
 final class ChatInitial extends ChatState {}
+
+class ChatSuccessState extends ChatState {
+  final List<MessageModel> messages;
+  ChatSuccessState({required this.messages});
+}
