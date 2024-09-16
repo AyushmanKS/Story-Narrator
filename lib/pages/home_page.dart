a2ad227b2ce6946e0e020cd1ce98cd0d514b68a0
AyushmanKS <ayushmanks1845@gmail.com>
2024-09-16 11:53:28 +0530

    initial commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/book.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(),
+      ),
+    );
+  }
+}
