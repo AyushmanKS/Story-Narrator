621a960f920841388e2805336953b0b8896aab90
AyushmanKS <ayushmanks1845@gmail.com>
2024-09-16 22:42:33 +0530

    update: home page bloc integration

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:story_narrator/bloc/chat_bloc.dart';
+import 'package:story_narrator/models/message_model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,83 +11,100 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final ChatBloc chatBloc = ChatBloc();
+  TextEditingController textEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.maxFinite,
-        height: double.maxFinite,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/book.png"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              height: 100,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  const Text(
-                    'Story Narrator',
-                    style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w900,
-                        color: Colors.white),
+      body: BlocConsumer<ChatBloc, ChatState>(
+        bloc: chatBloc,
+        listener: (context, state) {},
+        builder: (context, state) {
+          switch (state.runtimeType) {
+            case ChatSuccessState:
+              List<MessageModel> messages =
+                  (state as ChatSuccessState).messages;
+              return Container(
+                width: double.maxFinite,
+                height: double.maxFinite,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/book.png"),
+                    fit: BoxFit.cover,
                   ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.image_search,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: ListView(),
-            ),
-            Container(
-              padding: const EdgeInsets.all(16),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      style: const TextStyle(color: Colors.black),
-                      cursorColor: Theme.of(context).primaryColor,
-                      decoration: InputDecoration(
-                        fillColor: const Color(0xFFfde6d7),
-                        filled: true,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(100),
-                        ),
-                        focusColor: Theme.of(context).primaryColor,
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      height: 100,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          const Text(
+                            'Story Narrator',
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.w900,
+                                color: Colors.white),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.image_search,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                  const SizedBox(width: 15),
-                  const CircleAvatar(
-                    radius: 28,
-                    backgroundColor: Colors.brown,
-                    child: CircleAvatar(
-                      radius: 26,
-                      backgroundColor: Color(0xFFfde6d7),
-                      child: Icon(
-                        Icons.send,
-                        color: Colors.brown,
+                    Expanded(
+                      child: ListView(),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(16),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              style: const TextStyle(color: Colors.black),
+                              cursorColor: Theme.of(context).primaryColor,
+                              decoration: InputDecoration(
+                                fillColor: const Color(0xFFfde6d7),
+                                filled: true,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(100),
+                                ),
+                                focusColor: Theme.of(context).primaryColor,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 15),
+                          const CircleAvatar(
+                            radius: 28,
+                            backgroundColor: Colors.brown,
+                            child: CircleAvatar(
+                              radius: 26,
+                              backgroundColor: Color(0xFFfde6d7),
+                              child: Icon(
+                                Icons.send,
+                                color: Colors.brown,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+                  ],
+                ),
+              );
+
+            default:
+              return SizedBox();
+          }
+        },
       ),
     );
   }
